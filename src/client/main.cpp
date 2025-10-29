a1945eaf13f477efb951f3ad084869e5c5cf3936
RohanPhadnis <rohan.phadnis.usa@gmail.com>
2025-10-29 11:28:04 -0400

    sever client skeletons

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,3 +1,47 @@
+#include <iostream>
+
+#include <grpc/grpc.h>
+
+#include "../../protos/hpcfs_bench.pb.h"
+#include "../../protos/hpcfs_bench.grpc.pb.h"
+
+
+class ClusterClient : public grpc::ClientBidiReactor<hpcfs_bench::TestRequest, hpcfs_bench::TestResult> {
+private:
+    grpc::channel channel;
+    hpcfs_bench::ClusterClient::Stub stub;
+public:
+    ClusterClient() {
+        channel = grpc::CreateChannel("0.0.0.0:8000", grpc::InsecureChannelCredentials());
+        stub->async()->RouteChat(&context_, this);
+        StartCall(&channel);
+        std::cout << "Client created" << std::endl;
+    }
+    void OnDone() override {
+        std::cout << "Client done" << std::endl;
+    }
+    void OnReadDone(bool ok) override {
+        if (ok) {
+            hpcfs_bench::TestResult result = GetReadMessage();
+            std::cout << "Received TestResult: " << result.DebugString() << std::endl;
+            // Continue reading
+            StartRead();
+        } else {
+            std::cout << "No more TestResults from server." << std::endl;
+        }
+    }
+    void OnWriteDone(bool ok) override {
+        if (ok) {
+            std::cout << "TestRequest sent successfully." << std::endl;
+        } else {
+            std::cout << "Failed to send TestRequest." << std::endl;
+        }
+    }
+};
+
+
 int main() {
+    ClusterClient client;
+    Status status = client.Await();
     return 0;
 }
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,3 +1,52 @@
+#include <iostream>
+#include <memory>
+
+#include <grpc/grpc.h>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+
+#include "../../protos/hpcfs_bench.pb.h"
+#include "../../protos/hpcfs_bench.grpc.pb.h"
+
+
+class ClusterServiceReactor : public grpc::ServerBidiReactor<hpcfs_bench::TestRequest, hpcfs_bench::TestResult> {
+private:
+    bool active;
+public:
+    ClusterServiceReactor(): active(true) {
+        std::cout << "Reactor created" << std::endl;
+    }
+    void OnDone() override {
+        std::cout << "Reactor done" << std::endl;
+        active = false;
+    }
+};
+
+
+class ClusterService : public hpcfs_bench::ClusterService::Service {
+    ClusterService() {}
+    grpc::ServerBidiReactor< hpcfs_bench::TestRequest, hpcfs_bench::TestResult>* Comm(grpc::CallbackServerContext* context) {
+        ClusterServiceReactor* reactor = new ClusterServiceReactor();
+        return reactor;
+    }
+};
+
+
+void run_server() {
+    std::string server_address("0.0.0.0:8000");
+    ClusterService service;
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+
+    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    server->Wait();
+}
+
 int main() {
+    run_server();
+
     return 0;
 }
